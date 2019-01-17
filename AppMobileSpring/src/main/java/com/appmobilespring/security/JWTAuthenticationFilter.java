58d4367c6586e53f7f304216c2c7b42beb68e474
VictorMagalhaesSales <victormagalhaessales@gmail.com>
2019-01-17 09:41:48 -0300

    Expondo o header location nas respostas

--- AppMobileSpring/src/main/java/com/appmobilespring/security/JWTAuthenticationFilter.java
+++ AppMobileSpring/src/main/java/com/appmobilespring/security/JWTAuthenticationFilter.java
@@ -34,13 +34,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest req,
                                                 HttpServletResponse res) throws AuthenticationException {
-
 		try {
 			CredenciaisDTO creds = new ObjectMapper()
 	                .readValue(req.getInputStream(), CredenciaisDTO.class);
-
 	        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getSenha(), new ArrayList<>());
-
 	        Authentication auth = authenticationManager.authenticate(authToken);
 	        return auth;
 		}
@@ -54,10 +51,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                                             HttpServletResponse res,
                                             FilterChain chain,
                                             Authentication auth) throws IOException, ServletException {
-
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         res.addHeader("Authorization", "Bearer " + token);
+        res.addHeader("access-control-expose-headers", "Authorization");
 	}
 
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
