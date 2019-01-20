1a6397621929fe4824aabc3260921d7469580440
VictorMagalhaesSales <victormagalhaessales@gmail.com>
2019-01-20 05:58:59 -0300

    Adicionando a imagem dos produtos

--- AppMobileIonic/src/services/domain/produto.service.ts
+++ AppMobileIonic/src/services/domain/produto.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API_CONFIG } from '../../config/api.config';
-import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 
 @Injectable()
 export class ProdutoService {
@@ -9,7 +8,6 @@ export class ProdutoService {
   constructor(public http: HttpClient) {}
 
   findByCategoria(categoriaId : string) {
-    console.log(categoriaId);
     return this.http.get(`${API_CONFIG.baseUrl}/produtos/page/?categoriasid=${categoriaId}`);
   }
 }
