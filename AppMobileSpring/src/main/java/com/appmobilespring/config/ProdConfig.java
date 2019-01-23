0e06f54d1310e94b34a0e3c6161d0e084baad6c1
VictorMagalhaesSales <victormagalhaessales@gmail.com>
2019-01-23 05:36:02 -0300

    Adicionando o ProdConfig no spring

---
+++ AppMobileSpring/src/main/java/com/appmobilespring/config/ProdConfig.java
@@ -0,0 +1,40 @@
+package com.appmobilespring.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.appmobilespring.services.DBService;
+import com.appmobilespring.services.email.EmailService;
+import com.appmobilespring.services.email.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
