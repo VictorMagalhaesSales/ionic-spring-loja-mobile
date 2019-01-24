3cd346412fd87c521916d425516ce918be636fe3
VictorMagalhaesSales <victormagalhaessales@gmail.com>
2019-01-24 18:56:10 -0300

    Substituindo o backend(corrompido) e modificando URL do banco(heroku)

--- AppMobileIonic/src/config/api.config.ts
+++ AppMobileIonic/src/config/api.config.ts
@@ -1,3 +1,3 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "https://appmobilespring.herokuapp.com"
 }
--- AppMobileSpring/src/test/java/com/victorsales/appmobilespring/AppMobileSpringApplicationTests.java
+++ AppMobileSpring/src/test/java/com/victorsales/appmobilespring/AppMobileSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package com.appmobilespring;
+package com.victorsales.appmobilespring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class AppMobileSpringApplicationTests {
 	}
 
 }
+
